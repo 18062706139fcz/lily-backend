d4dced72b75691f2b57fd8ac55390fecbd146604
David-patrick-chuks <pd3072894@gmail.com>
2026-05-16 02:47:06 +0100

    feat: add HTTP server entrypoint

---
+++ src/server.ts
@@ -0,0 +1,35 @@
+import { createServer } from "node:http";
+
+import { createApp } from "./app";
+import { env } from "./config/env";
+import { logger } from "./config/logger";
+
+const app = createApp();
+const server = createServer(app);
+
+server.listen(env.PORT, () => {
+  logger.info(
+    {
+      port: env.PORT,
+      environment: env.NODE_ENV,
+    },
+    "Lily backend server started",
+  );
+});
+
+const shutdown = (signal: NodeJS.Signals) => {
+  logger.info({ signal }, "Graceful shutdown started");
+
+  server.close((error) => {
+    if (error) {
+      logger.error({ err: error }, "Error while shutting down server");
+      process.exit(1);
+    }
+
+    logger.info("HTTP server closed");
+    process.exit(0);
+  });
+};
+
+process.on("SIGINT", () => shutdown("SIGINT"));
+process.on("SIGTERM", () => shutdown("SIGTERM"));
